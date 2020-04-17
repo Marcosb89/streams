8d21f93e5abedfb3a99ddc387d6c2c2513912e8a
Marcosb89 <marcos.nirvana@gmail.com>
2020-04-17 12:40:04 -0300

    first commit

---
+++ client/src/components/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Router, Route } from "react-router-dom";
+//COMPONENTS
+import Header from "./Header";
+import StreamList from "./streams/StreamList";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamShow from "./streams/StreamShow";
+import history from "../history";
+
+const App = () => {
+	return (
+		<div className="container">
+			<Router history={history}>
+				<Header />
+				<div>
+					<Route exact path="/" component={StreamList} />
+					<Route exact path="/streams/new" component={StreamCreate} />
+					<Route exact path="/streams/edit/:id" component={StreamEdit} />
+					<Route exact path="/streams/delete" component={StreamDelete} />
+					<Route exact path="/streams/show" component={StreamShow} />
+				</div>
+			</Router>
+		</div>
+	);
+};
+
+export default App;
